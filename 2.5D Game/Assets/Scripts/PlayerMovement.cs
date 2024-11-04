500d10582cd0bb19083b4cb197aa88df795f0749
Daniel Acevedo <dacevedo23@gmail.com>
2024-11-04 07:46:47 -0800

    updates

--- 2.5D Game/Assets/Scripts/PlayerMovement.cs
+++ 2.5D Game/Assets/Scripts/PlayerMovement.cs
@@ -5,6 +5,7 @@ public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 5f;
     public float jumpForce = 5f;
+    public float climbSpeed = 3f;
     public Transform groundCheck;
     public LayerMask groundLayer;
 
@@ -30,7 +31,15 @@ public class PlayerController : MonoBehaviour
         // Allow jump if the jump button is pressed and the player is grounded
         if (context.performed && isGrounded)
         {
-            rb.velocity = new Vector3(rb.velocity.x, jumpForce, rb.velocity.z);  // Apply jump force on Y-axis
+            rb.velocity = new Vector3(rb.velocity.x, jumpForce, rb.velocity.z);
+        }
+    }
+
+    public void OnAttack(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            animator.SetTrigger("Attack");  // Trigger the Attack animation
         }
     }
 
@@ -41,13 +50,11 @@ public class PlayerController : MonoBehaviour
 
         // Grounded check using Physics.CheckSphere
         isGrounded = Physics.CheckSphere(groundCheck.position, 0.1f, groundLayer);
-
         isWalking = moveInput.x != 0;
         animator.SetBool("isWalking", isWalking);
 
         // Set IsJumping based on grounded state
         animator.SetBool("isJumping", !isGrounded);
-
 
         // Flip the player's direction based on movement
         if (moveInput.x > 0)
@@ -57,6 +64,6 @@ public class PlayerController : MonoBehaviour
         else if (moveInput.x < 0)
         {
             transform.localScale = new Vector3(-1, 1, 1);  // Facing left
-        } 
+        }
     }
 }
